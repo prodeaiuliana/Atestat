dd0b3594e6063a717399d8dbfa4f81ac13e3f790
Iulia Prodea <iuliaprodea@yahoo.com>
2018-11-08 07:52:02 +0200

    Initial commit

---
+++ mysqllogin/mysqllogin/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace mysqllogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PasswordInput = new System.Windows.Forms.TextBox();
+            this.UserInput = new System.Windows.Forms.TextBox();
+            this.username = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.Label();
+            this.login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PasswordInput
+            // 
+            this.PasswordInput.Location = new System.Drawing.Point(248, 127);
+            this.PasswordInput.Name = "PasswordInput";
+            this.PasswordInput.PasswordChar = '*';
+            this.PasswordInput.Size = new System.Drawing.Size(100, 20);
+            this.PasswordInput.TabIndex = 1;
+            this.PasswordInput.TextChanged += new System.EventHandler(this.PasswordInput_TextChanged);
+            // 
+            // UserInput
+            // 
+            this.UserInput.Location = new System.Drawing.Point(248, 61);
+            this.UserInput.Name = "UserInput";
+            this.UserInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.UserInput.Size = new System.Drawing.Size(100, 20);
+            this.UserInput.TabIndex = 2;
+            this.UserInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // username
+            // 
+            this.username.AutoSize = true;
+            this.username.Location = new System.Drawing.Point(79, 61);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(52, 13);
+            this.username.TabIndex = 3;
+            this.username.Text = "Usename";
+            this.username.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.Location = new System.Drawing.Point(79, 130);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(53, 13);
+            this.password.TabIndex = 4;
+            this.password.Text = "Password";
+            this.password.Click += new System.EventHandler(this.password_Click);
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(166, 205);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(75, 23);
+            this.login.TabIndex = 5;
+            this.login.Text = "Login";
+            this.login.UseVisualStyleBackColor = true;
+            this.login.Click += new System.EventHandler(this.login_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 319);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.UserInput);
+            this.Controls.Add(this.PasswordInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox PasswordInput;
+        private System.Windows.Forms.TextBox UserInput;
+        private System.Windows.Forms.Label username;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.Button login;
+    }
+}
+
---
+++ mysqllogin/mysqllogin/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data
+
+namespace mysqllogin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("works");
+        }
+
+        private void PasswordInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void password_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

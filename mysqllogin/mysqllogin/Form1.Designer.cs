e6cd2f7e233b4974552ad185bad0daa325b12ef6
Radu Tache <radutache76@gmail.com>
2018-11-08 23:16:27 +0200

    Functionality done

--- mysqllogin/mysqllogin/Form1.Designer.cs
+++ mysqllogin/mysqllogin/Form1.Designer.cs
@@ -28,30 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.PasswordInput = new System.Windows.Forms.TextBox();
-            this.UserInput = new System.Windows.Forms.TextBox();
+            this.passwordInput = new System.Windows.Forms.TextBox();
+            this.userInput = new System.Windows.Forms.TextBox();
             this.username = new System.Windows.Forms.Label();
             this.password = new System.Windows.Forms.Label();
             this.login = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // PasswordInput
+            // passwordInput
             // 
-            this.PasswordInput.Location = new System.Drawing.Point(248, 127);
-            this.PasswordInput.Name = "PasswordInput";
-            this.PasswordInput.PasswordChar = '*';
-            this.PasswordInput.Size = new System.Drawing.Size(100, 20);
-            this.PasswordInput.TabIndex = 1;
-            this.PasswordInput.TextChanged += new System.EventHandler(this.PasswordInput_TextChanged);
+            this.passwordInput.Location = new System.Drawing.Point(248, 127);
+            this.passwordInput.Name = "passwordInput";
+            this.passwordInput.PasswordChar = '*';
+            this.passwordInput.Size = new System.Drawing.Size(100, 20);
+            this.passwordInput.TabIndex = 1;
             // 
-            // UserInput
+            // userInput
             // 
-            this.UserInput.Location = new System.Drawing.Point(248, 61);
-            this.UserInput.Name = "UserInput";
-            this.UserInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.UserInput.Size = new System.Drawing.Size(100, 20);
-            this.UserInput.TabIndex = 2;
-            this.UserInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.userInput.Location = new System.Drawing.Point(248, 61);
+            this.userInput.Name = "userInput";
+            this.userInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.userInput.Size = new System.Drawing.Size(100, 20);
+            this.userInput.TabIndex = 2;
             // 
             // username
             // 
@@ -61,7 +59,6 @@
             this.username.Size = new System.Drawing.Size(52, 13);
             this.username.TabIndex = 3;
             this.username.Text = "Usename";
-            this.username.Click += new System.EventHandler(this.label1_Click);
             // 
             // password
             // 
@@ -71,7 +68,6 @@
             this.password.Size = new System.Drawing.Size(53, 13);
             this.password.TabIndex = 4;
             this.password.Text = "Password";
-            this.password.Click += new System.EventHandler(this.password_Click);
             // 
             // login
             // 
@@ -91,8 +87,8 @@
             this.Controls.Add(this.login);
             this.Controls.Add(this.password);
             this.Controls.Add(this.username);
-            this.Controls.Add(this.UserInput);
-            this.Controls.Add(this.PasswordInput);
+            this.Controls.Add(this.userInput);
+            this.Controls.Add(this.passwordInput);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -102,8 +98,8 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox PasswordInput;
-        private System.Windows.Forms.TextBox UserInput;
+        private System.Windows.Forms.TextBox passwordInput;
+        private System.Windows.Forms.TextBox userInput;
         private System.Windows.Forms.Label username;
         private System.Windows.Forms.Label password;
         private System.Windows.Forms.Button login;

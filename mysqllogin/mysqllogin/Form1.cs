3740554a9553d8d128ce8749b74ecb94b10b76c6
Radu Tache <radutache76@gmail.com>
2019-02-28 21:04:02 +0200

    Login works.

--- mysqllogin/mysqllogin/Form1.cs
+++ mysqllogin/mysqllogin/Form1.cs
@@ -1,40 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http;
 using MySql.Data.MySqlClient;
 
 namespace mysqllogin
 {
     public partial class Form1 : Form
     {
+        private static readonly HttpClient client = new HttpClient();
         public Form1()
         {
             InitializeComponent();
         }
-        
-        private MySqlConnection getDBConnection(string server, string username, string password, string database)
-        {
-            MySqlConnection connection = new MySqlConnection("server = " + server + "; database = " + database + "; user = " + username + "; password = " + password + "; ssl mode = none;");
-            try
-            {
-                connection.Open();
-            }
-            catch(MySqlException ex)
-            {
-                if(ex.Number == 1042)
-                {
-                    MessageBox.Show("Server is down.");
-                }
-            }
-            return connection;
-        }
-        private void login_Click(object sender, EventArgs e)
+        private async void login_Click(object sender, EventArgs e)
         {
             if(userInput.Text == "" || passwordInput.Text == "")
             {
@@ -42,17 +21,12 @@ namespace mysqllogin
             }
             else
             {
-                MySqlConnection conn = getDBConnection("localhost", "root", "rootpassword", "test");
-                if(conn.State == ConnectionState.Open)
+                try
                 {
-                    MySqlCommand cmd = new MySqlCommand("SELECT password FROM users WHERE username=@val1 AND password =@val2", conn);
-                    cmd.Parameters.AddWithValue("@val1", userInput.Text);
-                    cmd.Parameters.AddWithValue("@val2", passwordInput.Text);
-                    cmd.Prepare();
-                    MySqlDataReader res = cmd.ExecuteReader();
-                    if (res.Read())
+                    string res = await client.GetStringAsync("http://localhost:3000/login?name=" + userInput.Text + "&password=" + passwordInput.Text);
+                    if (Int32.Parse(res) >= 0)
                     {
-                        if(userInput.Text == "admin")
+                        if (res == "1")
                         {
                             Form2 admin = new Form2(this);
                             admin.Show();
@@ -69,6 +43,10 @@ namespace mysqllogin
                     {
                         MessageBox.Show("Login attempt failed.");
                     }
+                }
+                catch
+                {
+                    MessageBox.Show("Server is down.");
                 }
             }
         }
